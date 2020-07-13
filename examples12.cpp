83b4c3c61df8bffa398f6c990c1d4a0392497230
mingren <mingren.ms@autonavi.com>
2020-07-13 20:51:39 +0800

    add case

--- examples12.cpp
+++ examples12.cpp
@@ -18,13 +18,20 @@ int main( )
     const static int imageChannel = 3;
     const static int imageWidth = 512;
     const static int imageHeight = 288;
+    const static int64_t var64 = 100;
 
     printf("%d\n",imageChannel*imageWidth*imageHeight);
     printf("%Id\n",imageChannel*imageWidth*imageHeight);
     /*
-     * %I32d, 不能用lld。
+     * 32bit的数据 %I32d, 不能用lld。
      */
     printf("%I32d\n",imageChannel*imageWidth*imageHeight);
+
+    /*
+     * 64bit的数据用%lld或者%I64d
+     */
+    printf("%lld\n",var64);
+
 
     return 0;
 }
