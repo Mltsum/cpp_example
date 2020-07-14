458e42de68d54458beac62ca87e79e8c21a4ea36
mingren <mingren.ms@autonavi.com>
2020-07-14 10:44:58 +0800

    add case

--- examples4.cpp
+++ examples4.cpp
@@ -3,27 +3,32 @@
 #include <memory>
 
 /*
- * 智能指针unique_ptr申请固定长度的数组
+ *  1. 智能指针share_ptr的使用
+ *      std::shared_ptr 是通过指针保持对象共享所有权的智能指针。多个 shared_ptr 对象可占有同一对象
  */
-
-#include <io.h>
-#include <string>
 
 using namespace std;
 
-int main(){
-    std::unique_ptr<float> vars;
-    if(vars == nullptr){
-        cout << "ptr is nullptr" << endl;
+
+// 程序的主函数
+int main( )
+{
+    /*
+     * unique_ptr的数组智能指针，没有*和->操作，但支持下标操作[]
+     */
+    unique_ptr<int[]> up(new int[2]);
+
+    for (int i = 0; i < 2; i++) {
+        up[i] = i;
     }
+    cout << up[1] << endl;
 
     /*
-     * 申请固定长度数组内存
+     * shared_ptr的数组智能指针，有*和->操作，但不支持下标操作[]，只能通过get()去访问数组的元素。
+     * shared_ptr的数组智能指针，必须要自定义deleter
      */
-    std::unique_ptr<float> float_ptr(new float[10]);
-    for (int i = 0; i < 10; ++i) {
-        float_ptr[i] = i;
-        cout << float_ptr[i] << " " << endl;
-    }
+    shared_ptr<int> sp(new int[2], [](int* p){delete [] p;});
+    cout << "address is" << sp.get() << endl;
 
+    return 0;
 }
