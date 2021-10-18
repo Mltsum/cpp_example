0a94876a0b0b9b0a7fa8d488236198f288090a16
Mltsum <40685554+Mltsum@users.noreply.github.com>
2021-10-18 20:17:25 +0800

    [修改] 深入理解中介模式或者桥模式

--- designMode/中介模式/main.cpp
+++ designMode/中介模式/main.cpp
@@ -16,7 +16,7 @@
 using namespace std;
 
 /**
- * 设计模式之中介者模式
+ * 设计模式之中介者模式(桥模式)
  * 主要解决：对象与对象之间存在大量的关联关系，这样势必会导致系统的结构变得很复杂，同时若一个对象发生改变，我们也需要跟踪与之相关联的对象，同时做出相应的处理。
  * 何时使用：多个类相互耦合，形成了网状结构。
  * 如何解决：将上述网状结构分离为星型结构。
@@ -31,6 +31,8 @@ class Mediator;
  */
 class Object {
 public:
+    Object(){}
+    virtual ~Object(){}
     virtual void setMediator(Mediator* media) = 0;
     virtual void sendMsg(std::string &msg) = 0;
     virtual void receiveMsg(std::string &msg) = 0;
