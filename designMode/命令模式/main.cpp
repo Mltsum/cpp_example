e0e2026724b89ee9b9dc8a29344cca5c7c8aedb6
Mltsum <40685554+Mltsum@users.noreply.github.com>
2021-10-19 11:55:34 +0800

    [修改] 深入理解命令模式

--- designMode/命令模式/main.cpp
+++ designMode/命令模式/main.cpp
@@ -19,7 +19,10 @@ using namespace std;
  * 设计模式之命令模式
  *    将一个请求封装为一个对象，从而使你可用不同的请求对客户进行参数化, 对请求排队或记录请求日志，以及支持可撤销的操作。
  *    在OOP中，一切都是对象，将请求封装成对象，符合OOP的设计思想，当将客户的单个请求封装成对象以后，我们就可以对这个请求存储更多的信息，使请求拥有更多的能力。
- *    命令模式同样能够把请求发送者和接收者解耦，使得命令发送者不用去关心请求将以何种方式被处理。
+ *    命令模式同样能够把[请求发送者]和[接收者]解耦，使得命令发送者不用去关心请求将以何种方式被处理。
+ *
+ * 函数模版 和 此思路是一致的。
+ *
  */
 
 #define CHECK_AND_DELETE(ptr) { if(ptr){delete(ptr); (ptr)=NULL;} }
@@ -39,7 +42,7 @@ public:
 /**
  * 定义命令类
  */
-class Command{
+class Command{                      // 核心是这个虚接口，这样就方便用类来管理不同命令的对象。是命令模式可以执行的核心。
 public:
     Command(Detail *detail){
         this->mDetail = detail;
@@ -48,7 +51,7 @@ public:
     /**
      * 将command的执行做成接口开放
      */
-    virtual void excute() = 0;
+    virtual void excute() = 0;      // 虚接口规范，接口规范更严格。
 
 protected:
     Detail *mDetail;
@@ -118,13 +121,13 @@ int main(int argc, const char* argv[]) {
     /**
      * 定义命令操作的对象
      */
-    Detail *detail = new Detail();
+     Detail *detail = new Detail();
 
     /**
      * 定义不同类型的命令
      */
-    CommandStream *commandStream = new CommandStream(detail);
-    CommandText *commandText = new CommandText(detail);
+     CommandStream *commandStream = new CommandStream(detail);
+     CommandText *commandText = new CommandText(detail);
 
     /**
      * 定义命令管理的中间件
@@ -136,7 +139,7 @@ int main(int argc, const char* argv[]) {
      /**
       * 执行命令
       */
-    commandManager->printCommads();
+     commandManager->printCommads();
 
    return 0;
 }
